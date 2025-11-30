5f72751682a3a5d5617dd1c4687196caca1a0dab
szumaster3 <szumaster2009@protonmail.com>
2025-11-30 02:32:51 +0100

    Unify suitable for dyeing items

--- Server/src/main/content/data/Dyes.kt
+++ Server/src/main/content/data/Dyes.kt
@@ -2,21 +2,18 @@ package content.data
 
 import shared.consts.Items
 
-/**
- * Represents dyes with their item ids.
- */
-enum class Dyes(val id: Int) {
-    BLACK(Items.BLACK_MUSHROOM_INK_4622),
-    RED(Items.RED_DYE_1763),
-    YELLOW(Items.YELLOW_DYE_1765),
-    BLUE(Items.BLUE_DYE_1767),
-    ORANGE(Items.ORANGE_DYE_1769),
-    GREEN(Items.GREEN_DYE_1771),
-    PURPLE(Items.PURPLE_DYE_1773),
-    PINK(Items.PINK_DYE_6955);
+enum class Dyes(val dyeId: Int, val capeId: Int, val goblinMailId: Int) {
+    BLACK(Items.BLACK_MUSHROOM_INK_4622, Items.BLACK_CAPE_1019, Items.BLACK_GOBLIN_MAIL_9055),
+    RED(Items.RED_DYE_1763, Items.RED_CAPE_1007, Items.RED_GOBLIN_MAIL_9054),
+    YELLOW(Items.YELLOW_DYE_1765, Items.YELLOW_CAPE_1023, Items.YELLOW_GOBLIN_MAIL_9056),
+    BLUE(Items.BLUE_DYE_1767, Items.BLUE_CAPE_1021, Items.BLUE_GOBLIN_MAIL_287),
+    ORANGE(Items.ORANGE_DYE_1769, Items.ORANGE_CAPE_1031, Items.ORANGE_GOBLIN_MAIL_286),
+    GREEN(Items.GREEN_DYE_1771, Items.GREEN_CAPE_1027, Items.GREEN_GOBLIN_MAIL_9057),
+    PURPLE(Items.PURPLE_DYE_1773, Items.PURPLE_CAPE_1029, Items.PURPLE_GOBLIN_MAIL_9058),
+    PINK(Items.PINK_DYE_6955, Items.PINK_CAPE_6959, Items.PINK_GOBLIN_MAIL_9059);
 
     companion object {
-        private val idToDye = values().associateBy { it.id }
-        fun forId(id: Int) = idToDye[id]
+        private val ITEM_MAP = values().associateBy { it.dyeId }
+        fun forId(id: Int) = ITEM_MAP[id]
     }
 }
--- Server/src/main/content/global/plugins/interaction/with_item/ItemDyeingPlugin.kt
+++ Server/src/main/content/global/plugins/interaction/with_item/ItemDyeingPlugin.kt
@@ -1,6 +1,5 @@
 package content.global.plugins.interaction.with_item
 
-import content.data.Capes
 import content.data.Dyes
 import core.api.*
 import core.game.interaction.IntType
@@ -8,20 +7,20 @@ import core.game.interaction.InteractionListener
 import core.game.node.entity.player.Player
 import core.game.node.item.Item
 import core.game.world.update.flag.context.Animation
+import java.util.*
 import shared.consts.Animations
 import shared.consts.Items
-import java.util.*
 
 /**
  * Plugin handling item dyeing, including mixing dyes,
  * dyeing capes, and dyeing goblin armor.
  */
-class ItemDyeOptionPlugin : InteractionListener {
+class ItemDyeingPlugin : InteractionListener {
 
     companion object {
-        private val DYES = Dyes.values().map { it.id }.toIntArray()
-        private val CAPES = Capes.values().map { it.capeId }.toIntArray()
-        private val GOBLIN_MAIL = intArrayOf(Items.GOBLIN_MAIL_288, Items.RED_GOBLIN_MAIL_9054, Items.ORANGE_GOBLIN_MAIL_286, Items.YELLOW_GOBLIN_MAIL_9056, Items.GREEN_GOBLIN_MAIL_9057, Items.BLUE_GOBLIN_MAIL_287, Items.PURPLE_GOBLIN_MAIL_9058, Items.BLACK_GOBLIN_MAIL_9055, Items.PINK_GOBLIN_MAIL_9059)
+        private val DYES = Dyes.values().map { it.dyeId }.toIntArray()
+        private val CAPES = Dyes.values().map { it.capeId }.toIntArray()
+        private val GOBLIN_MAIL = Dyes.values().map { it.goblinMailId }.toIntArray()
     }
 
     override fun defineListeners() {
@@ -40,13 +39,14 @@ class ItemDyeOptionPlugin : InteractionListener {
          */
 
         onUseWith(IntType.ITEM, DYES, *CAPES) { player, used, with ->
-            Capes.forDyeId(used.id)?.let { cape ->
+            val dye = Dyes.forId(used.id)
+            if (dye != null && dye.capeId == with.id) {
                 if (!removeItem(player, used.id)) return@onUseWith false
-                replaceSlot(player, with.index, Item(cape.capeId))
-                player.sendMessage("You dye the cape.")
+                replaceSlot(player, with.index, Item(dye.capeId))
+                sendMessage(player, "You dye the cape.")
                 return@onUseWith true
-            } ?: run {
-                player.sendMessage("This dye cannot be used with this cape.")
+            } else {
+                sendMessage(player, "This dye cannot be used with this cape.")
                 return@onUseWith false
             }
         }
@@ -59,7 +59,7 @@ class ItemDyeOptionPlugin : InteractionListener {
             if (with.id == Items.GOBLIN_MAIL_288) {
                 dyeGoblinMail(player, used.id, with.id, with.index)
             } else {
-                player.sendMessage("That item is already dyed.")
+                sendMessage(player, "That item is already dyed.")
             }
             return@onUseWith true
         }
@@ -68,8 +68,8 @@ class ItemDyeOptionPlugin : InteractionListener {
          * Handles message when trying to wear goblin armor.
          */
 
-        onEquip(GOBLIN_MAIL){ player, _ ->
-            sendMessage(player,"That armour is too small for a human.")
+        onEquip(GOBLIN_MAIL) { player, _ ->
+            sendMessage(player, "That armour is too small for a human.")
             return@onEquip false
         }
     }
@@ -78,28 +78,28 @@ class ItemDyeOptionPlugin : InteractionListener {
      * Mixes two different dyes to create a new color.
      */
     private fun handleDyeCombine(player: Player, primaryId: Int, secondaryId: Int): Boolean {
-        val firstColor = Dyes.forId(primaryId) ?: return false
-        val secondColor = Dyes.forId(secondaryId) ?: return false
-        if (firstColor == secondColor) return false
+        val first = Dyes.forId(primaryId) ?: return false
+        val second = Dyes.forId(secondaryId) ?: return false
+        if (first == second) return false
 
-        val mix = when (setOf(firstColor, secondColor)) {
-            setOf(Dyes.RED, Dyes.YELLOW) -> Dyes.ORANGE
-            setOf(Dyes.YELLOW, Dyes.BLUE) -> Dyes.GREEN
-            setOf(Dyes.RED, Dyes.BLUE) -> Dyes.PURPLE
-            else -> null
-        } ?: return sendMessage(player, "Those dyes don't mix together.").let { false }
+        val mix =
+            when (setOf(first, second)) {
+                setOf(Dyes.RED, Dyes.YELLOW) -> Dyes.ORANGE
+                setOf(Dyes.YELLOW, Dyes.BLUE) -> Dyes.GREEN
+                setOf(Dyes.RED, Dyes.BLUE) -> Dyes.PURPLE
+                else -> return sendMessage(player, "Those dyes don't mix together.").let { false }
+            }
 
-        if (!inInventory(player, firstColor.id) || !inInventory(player, secondColor.id)) {
+        if (!inInventory(player, first.dyeId) || !inInventory(player, second.dyeId)) {
             sendMessage(player, "You don't have the required dyes to mix.")
             return false
         }
 
         val article = if (mix.name.first().lowercaseChar() in "aeiou") "an" else "a"
-        if(removeItem(player, firstColor) && removeItem(player, secondColor))
-        {
+        if (removeItem(player, first.dyeId) && removeItem(player, second.dyeId)) {
             player.animate(Animation(Animations.DYE_COMBINE_4348))
             sendMessage(player, "You mix the two dyes and make $article ${mix.name.lowercase()} dye.")
-            addItemOrDrop(player, mix.id)
+            addItemOrDrop(player, mix.dyeId)
         }
 
         return true
@@ -113,7 +113,7 @@ class ItemDyeOptionPlugin : InteractionListener {
         if (mailId != Items.GOBLIN_MAIL_288) return false
 
         val productId = GOBLIN_MAIL.getOrNull(dye.ordinal) ?: return false
-        if (!removeItem(player, Item(dye.id))) return false
+        if (!removeItem(player, Item(dye.dyeId))) return false
 
         replaceSlot(player, mailSlot, Item(productId))
         player.sendMessage("You dye the goblin armour ${dye.name.lowercase(Locale.getDefault())}.")
