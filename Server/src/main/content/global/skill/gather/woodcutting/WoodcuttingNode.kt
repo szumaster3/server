f3d040e63aec47b5d9861534f70c093c0ba2df42
szumaster3 <szumaster2009@protonmail.com>
2025-12-18 00:18:14 +0100

    Misc improvements and fixes

--- Server/src/main/content/global/skill/gather/woodcutting/WoodcuttingNode.kt
+++ Server/src/main/content/global/skill/gather/woodcutting/WoodcuttingNode.kt
@@ -5,512 +5,109 @@ import core.game.world.repository.Repository.players
 import shared.consts.Scenery
 
 /**
- * The enum Woodcutting node.
+ * Represents the woodcutting node.
  */
 enum class WoodcuttingNode {
-    /**
-     * Standard tree 1 woodcutting node.
-     */
     STANDARD_TREE_1(Scenery.TREE_1276, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 2 woodcutting node.
-     */
     STANDARD_TREE_2(Scenery.TREE_1277, Scenery.TREE_STUMP_1343, 1.toByte()),
-
-    /**
-     * Standard tree 3 woodcutting node.
-     */
     STANDARD_TREE_3(Scenery.TREE_1278, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 4 woodcutting node.
-     */
     STANDARD_TREE_4(Scenery.TREE_1279, Scenery.TREE_STUMP_1345, 1.toByte()),
-
-    /**
-     * Standard tree 5 woodcutting node.
-     */
     STANDARD_TREE_5(Scenery.TREE_1280, Scenery.TREE_STUMP_1343, 1.toByte()),
-
-    /**
-     * Standard tree 6 woodcutting node.
-     */
     STANDARD_TREE_6(Scenery.TREE_1330, Scenery.TREE_STUMP_1341, 1.toByte()),
-
-    /**
-     * Standard tree 7 woodcutting node.
-     */
     STANDARD_TREE_7(Scenery.TREE_1331, Scenery.TREE_STUMP_1341, 1.toByte()),
-
-    /**
-     * Standard tree 8 woodcutting node.
-     */
     STANDARD_TREE_8(Scenery.TREE_1332, Scenery.TREE_STUMP_1341, 1.toByte()),
-
-    /**
-     * Standard tree 9 woodcutting node.
-     */
     STANDARD_TREE_9(Scenery.TREE_2409, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 10 woodcutting node.
-     */
     STANDARD_TREE_10(Scenery.TUTORIAL_TREE_3033, Scenery.TREE_STUMP_1345, 1.toByte()),
-
-    /**
-     * Standard tree 11 woodcutting node.
-     */
     STANDARD_TREE_11(Scenery.TREE_3034, Scenery.TREE_STUMP_1345, 1.toByte()),
-
-    /**
-     * Standard tree 12 woodcutting node.
-     */
     STANDARD_TREE_12(Scenery.TREE_3035, Scenery.TREE_STUMP_1347, 1.toByte()),
-
-    /**
-     * Standard tree 13 woodcutting node.
-     */
     STANDARD_TREE_13(Scenery.TREE_3036, Scenery.TREE_STUMP_1351, 1.toByte()),
-
-    /**
-     * Standard tree 14 woodcutting node.
-     */
     STANDARD_TREE_14(Scenery.TREE_3879, Scenery.TREE_STUMP_3880, 1.toByte()),
-
-    /**
-     * Standard tree 15 woodcutting node.
-     */
     STANDARD_TREE_15(Scenery.TREE_3881, Scenery.TREE_STUMP_3880, 1.toByte()),
-
-    /**
-     * Standard tree 16 woodcutting node.
-     */
     STANDARD_TREE_16(Scenery.TREE_3882, Scenery.TREE_STUMP_3880, 1.toByte()),
-
-    /**
-     * Standard tree 17 woodcutting node.
-     */
     STANDARD_TREE_17(Scenery.TREE_3883, Scenery.TREE_STUMP_3884, 1.toByte()),
-
-    /**
-     * Standard tree 18 woodcutting node.
-     */
     STANDARD_TREE_18(Scenery.TREE_10041, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 19 woodcutting node.
-     */
     STANDARD_TREE_19(Scenery.TREE_14308, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 20 woodcutting node.
-     */
     STANDARD_TREE_20(Scenery.TREE_14309, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 21 woodcutting node.
-     */
     STANDARD_TREE_21(Scenery.TREE_16264, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 22 woodcutting node.
-     */
     STANDARD_TREE_22(Scenery.TREE_16265, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 23 woodcutting node.
-     */
     STANDARD_TREE_23(Scenery.TREE_30132, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 24 woodcutting node.
-     */
     STANDARD_TREE_24(Scenery.TREE_30133, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 25 woodcutting node.
-     */
     STANDARD_TREE_25(Scenery.TREE_37477, Scenery.TREE_STUMP_1342, 1.toByte()),
-
-    /**
-     * Standard tree 26 woodcutting node.
-     */
     STANDARD_TREE_26(Scenery.TREE_37478, Scenery.TREE_STUMP_37653, 1.toByte()),
-
-    /**
-     * Standard tree 27 woodcutting node.
-     */
     STANDARD_TREE_27(Scenery.TREE_37652, Scenery.TREE_STUMP_37653, 1.toByte()),
-
-    /**
-     * Dead tree 1 woodcutting node.
-     */
     DEAD_TREE_1(Scenery.DEAD_TREE_1282, Scenery.TREE_STUMP_1347, 2.toByte()),
-
-    /**
-     * Dead tree 2 woodcutting node.
-     */
     DEAD_TREE_2(Scenery.DEAD_TREE_1283, Scenery.TREE_STUMP_1347, 2.toByte()),
-
-    /**
-     * Dead tree 3 woodcutting node.
-     */
     DEAD_TREE_3(Scenery.DEAD_TREE_1284, Scenery.TREE_STUMP_1348, 2.toByte()),
-
-    /**
-     * Dead tree 4 woodcutting node.
-     */
     DEAD_TREE_4(Scenery.DEAD_TREE_1285, Scenery.TREE_STUMP_1349, 2.toByte()),
-
-    /**
-     * Dead tree 5 woodcutting node.
-     */
     DEAD_TREE_5(Scenery.DEAD_TREE_1286, Scenery.TREE_STUMP_1351, 2.toByte()),
-
-    /**
-     * Dead tree 6 woodcutting node.
-     */
     DEAD_TREE_6(Scenery.DEAD_TREE_1289, Scenery.TREE_STUMP_1353, 2.toByte()),
-
-    /**
-     * Dead tree 7 woodcutting node.
-     */
     DEAD_TREE_7(Scenery.DEAD_TREE_1290, Scenery.TREE_STUMP_1354, 2.toByte()),
-
-    /**
-     * Dead tree 8 woodcutting node.
-     */
     DEAD_TREE_8(Scenery.DEAD_TREE_1291, Scenery.TREE_STUMP_23054, 2.toByte()),
-
-    /**
-     * Dead tree 9 woodcutting node.
-     */
     DEAD_TREE_9(Scenery.DEAD_TREE_1365, Scenery.TREE_STUMP_1352, 2.toByte()),
-
-    /**
-     * Dead tree 10 woodcutting node.
-     */
     DEAD_TREE_10(Scenery.DEAD_TREE_1383, Scenery.TREE_STUMP_1358, 2.toByte()),
-
-    /**
-     * Dead tree 11 woodcutting node.
-     */
     DEAD_TREE_11(Scenery.DEAD_TREE_1384, Scenery.TREE_STUMP_1359, 2.toByte()),
-
-    /**
-     * Dead tree 12 woodcutting node.
-     */
     DEAD_TREE_12(Scenery.DEAD_TREE_5902, Scenery.TREE_STUMP_1347, 2.toByte()),
-
-    /**
-     * Dead tree 13 woodcutting node.
-     */
     DEAD_TREE_13(Scenery.DEAD_TREE_5903, Scenery.TREE_STUMP_1353, 2.toByte()),
-
-    /**
-     * Dead tree 14 woodcutting node.
-     */
     DEAD_TREE_14(Scenery.DEAD_TREE_5904, Scenery.TREE_STUMP_1353, 2.toByte()),
-
-    /**
-     * Dead tree 15 woodcutting node.
-     */
     DEAD_TREE_15(Scenery.DEAD_TREE_32294, Scenery.TREE_STUMP_1353, 2.toByte()),
-
-    /**
-     * Dead tree 16 woodcutting node.
-     */
     DEAD_TREE_16(Scenery.DEAD_TREE_37481, Scenery.TREE_STUMP_1347, 2.toByte()),
-
-    /**
-     * Dead tree 17 woodcutting node.
-     */
     DEAD_TREE_17(Scenery.DEAD_TREE_37482, Scenery.TREE_STUMP_1351, 2.toByte()),
-
-    /**
-     * Dead tree 18 woodcutting node.
-     */
     DEAD_TREE_18(Scenery.DEAD_TREE_37483, Scenery.TREE_STUMP_1358, 2.toByte()),
-
-    /**
-     * Dead tree 19 woodcutting node.
-     */
     DEAD_TREE_19(Scenery.DYING_TREE_24168, Scenery.DYING_TREE_STUMP_24169, 2.toByte()),
-
-    /**
-     * Evergreen 1 woodcutting node.
-     */
     EVERGREEN_1(Scenery.EVERGREEN_1315, Scenery.TREE_STUMP_1342, 3.toByte()),
-
-    /**
-     * Evergreen 2 woodcutting node.
-     */
     EVERGREEN_2(Scenery.EVERGREEN_1316, Scenery.TREE_STUMP_1355, 3.toByte()),
-
-    /**
-     * Evergreen 3 woodcutting node.
-     */
     EVERGREEN_3(Scenery.EVERGREEN_1318, Scenery.TREE_STUMP_1355, 3.toByte()),
-
-    /**
-     * Evergreen 4 woodcutting node.
-     */
     EVERGREEN_4(Scenery.EVERGREEN_1319, Scenery.TREE_STUMP_1355, 3.toByte()),
-
-    /**
-     * Jungle tree 1 woodcutting node.
-     */
     JUNGLE_TREE_1(Scenery.JUNGLE_TREE_2887, 0, 4.toByte()),
-
-    /**
-     * Jungle tree 2 woodcutting node.
-     */
     JUNGLE_TREE_2(Scenery.JUNGLE_TREE_2889, Scenery.JUNGLE_TREE_STUMP_4819, 4.toByte()),
-
-    /**
-     * Jungle tree 3 woodcutting node.
-     */
     JUNGLE_TREE_3(Scenery.JUNGLE_TREE_2890, Scenery.JUNGLE_TREE_STUMP_4821, 4.toByte()),
-
-    /**
-     * Jungle tree 4 woodcutting node.
-     */
     JUNGLE_TREE_4(Scenery.JUNGLE_TREE_4818, Scenery.JUNGLE_TREE_STUMP_4819, 4.toByte()),
-
-    /**
-     * Jungle tree 5 woodcutting node.
-     */
     JUNGLE_TREE_5(Scenery.JUNGLE_TREE_4820, Scenery.JUNGLE_TREE_STUMP_4821, 4.toByte()),
-
-    /**
-     * Jungle bush 1 woodcutting node.
-     */
     JUNGLE_BUSH_1(Scenery.JUNGLE_BUSH_2892, Scenery.SLASHED_BUSH_2894, 5.toByte()),
-
-    /**
-     * Jungle bush 2 woodcutting node.
-     */
     JUNGLE_BUSH_2(Scenery.JUNGLE_BUSH_2893, Scenery.SLASHED_BUSH_2895, 5.toByte()),
-
-    /**
-     * Achey tree woodcutting node.
-     */
     ACHEY_TREE(Scenery.ACHEY_TREE_2023, Scenery.ACHEY_TREE_STUMP_3371, 6.toByte()),
-
-    /**
-     * Oak tree 1 woodcutting node.
-     */
     OAK_TREE_1(Scenery.OAK_1281, Scenery.TREE_STUMP_1356, 7.toByte()),
-
-    /**
-     * Oak tree 2 woodcutting node.
-     */
     OAK_TREE_2(Scenery.OAK_3037, Scenery.TREE_STUMP_1357, 7.toByte()),
-
-    /**
-     * Oak tree 3 woodcutting node.
-     */
     OAK_TREE_3(Scenery.OAK_37479, Scenery.TREE_STUMP_1356, 7.toByte()),
-
-    /**
-     * Oak tree 4 woodcutting node.
-     */
     OAK_TREE_4(Scenery.OAK_8467, Scenery.TREE_STUMP_1356, 19.toByte(), true),
-
-    /**
-     * Willow tree 1 woodcutting node.
-     */
     WILLOW_TREE_1(Scenery.WILLOW_1308, Scenery.TREE_STUMP_7399, 8.toByte()),
-
-    /**
-     * Willow tree 2 woodcutting node.
-     */
     WILLOW_TREE_2(Scenery.WILLOW_5551, Scenery.TREE_STUMP_5554, 8.toByte()),
-
-    /**
-     * Willow tree 3 woodcutting node.
-     */
     WILLOW_TREE_3(Scenery.WILLOW_5552, Scenery.TREE_STUMP_5554, 8.toByte()),
-
-    /**
-     * Willow tree 4 woodcutting node.
-     */
     WILLOW_TREE_4(Scenery.WILLOW_5553, Scenery.TREE_STUMP_5554, 8.toByte()),
-
-    /**
-     * Willow tree 5 woodcutting node.
-     */
     WILLOW_TREE_5(Scenery.WILLOW_37480, Scenery.TREE_STUMP_7399, 8.toByte()),
-
-    /**
-     * Willow tree 6 woodcutting node.
-     */
     WILLOW_TREE_6(Scenery.WILLOW_TREE_8488, Scenery.TREE_STUMP_7399, 20.toByte(), true),
-
-    /**
-     * Teak 1 woodcutting node.
-     */
     TEAK_1(Scenery.TEAK_9036, Scenery.TREE_STUMP_9037, 9.toByte()),
-
-    /**
-     * Teak 2 woodcutting node.
-     */
     TEAK_2(Scenery.TEAK_15062, Scenery.TREE_STUMP_9037, 9.toByte()),
-
-    /**
-     * Maple tree 1 woodcutting node.
-     */
     MAPLE_TREE_1(Scenery.MAPLE_TREE_1307, Scenery.TREE_STUMP_7400, 10.toByte()),
-
-    /**
-     * Maple tree 2 woodcutting node.
-     */
     MAPLE_TREE_2(Scenery.MAPLE_TREE_4674, Scenery.TREE_STUMP_7400, 10.toByte()),
-
-    /**
-     * Maple tree 3 woodcutting node.
-     */
     MAPLE_TREE_3(Scenery.MAPLE_TREE_8444, Scenery.TREE_STUMP_7400, 21.toByte(), true),
-
-    /**
-     * Hollow tree 1 woodcutting node.
-     */
     HOLLOW_TREE_1(Scenery.HOLLOW_TREE_2289, Scenery.TREE_STUMP_2310, 11.toByte()),
-
-    /**
-     * Hollow tree 2 woodcutting node.
-     */
     HOLLOW_TREE_2(Scenery.HOLLOW_TREE_4060, Scenery.TREE_STUMP_4061, 11.toByte()),
-
-    /**
-     * Mahogany woodcutting node.
-     */
     MAHOGANY(Scenery.MAHOGANY_9034, Scenery.TREE_STUMP_9035, 12.toByte()),
-
-    /**
-     * Swaying tree woodcutting node.
-     */
     SWAYING_TREE(Scenery.SWAYING_TREE_4142, -1, 30.toByte()),
-
-    /**
-     * Arctic pine woodcutting node.
-     */
     ARCTIC_PINE(Scenery.ARCTIC_PINE_21273, Scenery.TREE_STUMP_21274, 13.toByte()),
-
-    /**
-     * Eucalyptus 1 woodcutting node.
-     */
     EUCALYPTUS_1(Scenery.EUCALYPTUS_TREE_28951, Scenery.EUCALYPTUS_STUMP_28954, 14.toByte()),
-
-    /**
-     * Eucalyptus 2 woodcutting node.
-     */
     EUCALYPTUS_2(Scenery.EUCALYPTUS_TREE_28952, Scenery.EUCALYPTUS_STUMP_28955, 14.toByte()),
-
-    /**
-     * Eucalyptus 3 woodcutting node.
-     */
     EUCALYPTUS_3(Scenery.EUCALYPTUS_TREE_28953, Scenery.EUCALYPTUS_STUMP_28956, 14.toByte()),
-
-    /**
-     * Yew woodcutting node.
-     */
     YEW(Scenery.YEW_1309, Scenery.TREE_STUMP_7402, 15.toByte()),
-
-    /**
-     * Yew 1 woodcutting node.
-     */
     YEW_1(Scenery.YEW_TREE_8513, Scenery.TREE_STUMP_7402, 22.toByte(), true),
-
-    /**
-     * Magic tree 1 woodcutting node.
-     */
     MAGIC_TREE_1(Scenery.MAGIC_TREE_1306, Scenery.TREE_STUMP_7401, 16.toByte()),
-
-    /**
-     * Magic tree 2 woodcutting node.
-     */
     MAGIC_TREE_2(Scenery.MAGIC_TREE_37823, Scenery.TREE_STUMP_37824, 16.toByte()),
-
-    /**
-     * Magic tree 3 woodcutting node.
-     */
     MAGIC_TREE_3(Scenery.MAGIC_TREE_8409, Scenery.TREE_STUMP_37824, 23.toByte(), true),
-
-    /**
-     * Cursed magic tree woodcutting node.
-     */
     CURSED_MAGIC_TREE(Scenery.CURSED_MAGIC_TREE_37821, Scenery.TREE_STUMP_37822, 17.toByte()),
-
-    /**
-     * Dramen tree woodcutting node.
-     */
     DRAMEN_TREE(Scenery.DRAMEN_TREE_1292, 771, 18.toByte()),
-
-    /**
-     * Windswept tree woodcutting node.
-     */
     WINDSWEPT_TREE(Scenery.WINDSWEPT_TREE_18137, Scenery.TREE_STUMP_1353, 19.toByte()),
-
-    /**
-     * Light jungle 1 woodcutting node.
-     */
     LIGHT_JUNGLE_1(Scenery.LIGHT_JUNGLE_9010, Scenery.LIGHT_JUNGLE_9010, 31.toByte()),
-
-    /**
-     * Light jungle 2 woodcutting node.
-     */
     LIGHT_JUNGLE_2(Scenery.LIGHT_JUNGLE_9011, Scenery.LIGHT_JUNGLE_9010, 31.toByte()),
-
-    /**
-     * Light jungle 3 woodcutting node.
-     */
     LIGHT_JUNGLE_3(Scenery.LIGHT_JUNGLE_9012, Scenery.LIGHT_JUNGLE_9010, 31.toByte()),
-
-    /**
-     * Light jungle 4 woodcutting node.
-     */
     LIGHT_JUNGLE_4(Scenery.LIGHT_JUNGLE_9013, Scenery.LIGHT_JUNGLE_9010, 31.toByte()),
-
-    /**
-     * Medium jungle 1 woodcutting node.
-     */
     MEDIUM_JUNGLE_1(Scenery.MEDIUM_JUNGLE_9015, Scenery.MEDIUM_JUNGLE_9015, 32.toByte()),
-
-    /**
-     * Medium jungle 2 woodcutting node.
-     */
     MEDIUM_JUNGLE_2(Scenery.MEDIUM_JUNGLE_9016, Scenery.MEDIUM_JUNGLE_9015, 32.toByte()),
-
-    /**
-     * Medium jungle 3 woodcutting node.
-     */
     MEDIUM_JUNGLE_3(Scenery.MEDIUM_JUNGLE_9017, Scenery.MEDIUM_JUNGLE_9015, 32.toByte()),
-
-    /**
-     * Medium jungle 4 woodcutting node.
-     */
     MEDIUM_JUNGLE_4(Scenery.MEDIUM_JUNGLE_9018, Scenery.MEDIUM_JUNGLE_9015, 32.toByte()),
-
-    /**
-     * Dense jungle 1 woodcutting node.
-     */
     DENSE_JUNGLE_1(Scenery.DENSE_JUNGLE_9020, Scenery.DENSE_JUNGLE_9020, 33.toByte()),
-
-    /**
-     * Dense jungle 2 woodcutting node.
-     */
     DENSE_JUNGLE_2(Scenery.DENSE_JUNGLE_9021, Scenery.DENSE_JUNGLE_9020, 33.toByte()),
-
-    /**
-     * Dense jungle 3 woodcutting node.
-     */
     DENSE_JUNGLE_3(Scenery.DENSE_JUNGLE_9022, Scenery.DENSE_JUNGLE_9020, 33.toByte()),
-
-    /**
-     * Dense jungle 4 woodcutting node.
-     */
     DENSE_JUNGLE_4(Scenery.DENSE_JUNGLE_9023, Scenery.DENSE_JUNGLE_9020, 33.toByte());
 
     /**
