c680f39d684c024d96f552d701898cf47711f433
szumaster3 <szumaster2009@protonmail.com>
2025-12-23 08:07:00 +0100

    Fix spinning & add spinning scenery animation

--- Server/src/main/content/global/skill/crafting/SpinningPlugin.kt
+++ Server/src/main/content/global/skill/crafting/SpinningPlugin.kt
@@ -8,20 +8,19 @@ import core.game.node.entity.player.link.diary.DiaryManager
 import core.game.node.entity.player.link.diary.DiaryType
 import core.game.node.entity.skill.Skills
 import core.game.node.item.Item
+import core.game.node.scenery.Scenery
 import core.game.world.map.Location
-import shared.consts.Animations
-import shared.consts.Components
-import shared.consts.Items
-import shared.consts.Sounds
+import shared.consts.*
 
 class SpinningPlugin : InteractionListener, InterfaceListener {
+
     override fun defineListeners() {
 
         /*
          * Handles interaction with spinning wheel.
          */
-
-        on(CraftingDefinition.SPINNING_WHEEL, IntType.SCENERY, "spin") { player, _ ->
+        on(CraftingDefinition.SPINNING_WHEEL, IntType.SCENERY, "spin") { player, obj ->
+            setAttribute(player, "current_spinning_wheel", obj)
             openInterface(player, Components.CRAFTING_SPINNING_459)
             return@on true
         }
@@ -29,11 +28,13 @@ class SpinningPlugin : InteractionListener, InterfaceListener {
         /*
          * Handles creating golden wool.
          */
-
-        onUseWith(IntType.SCENERY, Items.GOLDEN_FLEECE_3693, *CraftingDefinition.SPINNING_WHEEL) { player, _, _ ->
+        onUseWith(IntType.SCENERY, Items.GOLDEN_FLEECE_3693, *CraftingDefinition.SPINNING_WHEEL) { player, _, with ->
             if (removeItem(player, Items.GOLDEN_FLEECE_3693)) {
+                lock(player, 3)
+                playAudio(player, Sounds.SPINNING_2590)
+                animate(player, 894)
+                animateScenery(with.asScenery(), 466)
                 addItem(player, Items.GOLDEN_WOOL_3694)
-                animate(player, Animations.OLD_COOK_RANGE_896)
                 sendDialogue(player, "You spin the Golden Fleece into a ball of Golden Wool.")
             }
             return@onUseWith true
@@ -48,27 +49,32 @@ class SpinningPlugin : InteractionListener, InterfaceListener {
                 return@on true
             }
 
-            var amount = when (opcode) {
+            val wheel = getAttribute<Scenery>(player, "current_spinning_wheel", Scenery(0,Location(0,0,0))) ?: run {
+                return@on true
+            }
+
+            val amount = when (opcode) {
                 155 -> 1
                 196 -> 5
                 124 -> amountInInventory(player, spin.need)
                 199 -> {
                     sendInputDialogue(player, true, "Enter the amount:") { value: Any ->
                         val valAmount = if (value is String) value.toInt() else value as Int
-                        handleSpinning(player, spin, valAmount)
+                        closeInterface(player)
+                        handleSpinning(player, spin, wheel, valAmount)
                     }
                     return@on true
                 }
-
                 else -> 1
             }
 
-            handleSpinning(player, spin, amount)
+            closeInterface(player)
+            handleSpinning(player, spin, wheel, amount)
             return@on true
         }
     }
 
-    private fun handleSpinning(player: Player, spin: CraftingDefinition.Spinning, amount: Int) {
+    private fun handleSpinning(player: Player, spin: CraftingDefinition.Spinning, wheel: core.game.node.scenery.Scenery, amount: Int) {
         if (!clockReady(player, Clocks.SKILLING)) return
 
         var remaining = amount
@@ -92,6 +98,7 @@ class SpinningPlugin : InteractionListener, InterfaceListener {
 
             playAudio(player, Sounds.SPINNING_2590)
             animate(player, 894)
+            animateScenery(wheel, 466)
             delayClock(player, Clocks.SKILLING, delay)
 
             if (removeItem(player, Item(spin.need, 1))) {
@@ -101,12 +108,13 @@ class SpinningPlugin : InteractionListener, InterfaceListener {
             }
 
             // Seers diary.
+            val spun = getAttribute<Int>(player, "diary:seers:bowstrings-spun", 0)
             if (player.viewport.region!!.id == 10806 && !hasDiaryTaskComplete(player, DiaryType.SEERS_VILLAGE, 0, 4)) {
-                if (player.getAttribute("diary:seers:bowstrings-spun", 0) >= 4) {
+                if (spun >= 4) {
                     setAttribute(player, "/save:diary:seers:bowstrings-spun", 5)
                     finishDiaryTask(player, DiaryType.SEERS_VILLAGE, 0, 4)
                 } else {
-                    setAttribute(player, "/save:diary:seers:bowstrings-spun", getAttribute(player, "diary:seers:bowstrings-spun", 0) + 1)
+                    setAttribute(player, "/save:diary:seers:bowstrings-spun", spun + 1)
                 }
             }
 
@@ -114,7 +122,6 @@ class SpinningPlugin : InteractionListener, InterfaceListener {
                 delayClock(player, Clocks.SKILLING, delay)
                 setCurrentScriptState(player, 0)
                 delayScript(player, delay)
-                true
             } else false
         }
     }
