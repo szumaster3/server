c3aebcd9ad9899a2cf6c2d77626f4add1de73e14
szumaster3 <szumaster2009@protonmail.com>
2025-12-23 04:30:51 +0100

    Fix imports for StanfordDialogue

--- Server/src/main/content/region/kandarin/seers_village/quest/murder/dialogue/StanfordDialogue.kt
+++ Server/src/main/content/region/kandarin/seers_village/quest/murder/dialogue/StanfordDialogue.kt
@@ -1,14 +1,14 @@
 package content.region.kandarin.seers_village.quest.murder.dialogue
 
-import content.data.Quests
-import content.region.kandarin.seers_village.quest.murder.attributeNoiseClue
-import content.region.kandarin.seers_village.quest.murder.attributePoisonClue
-import core.api.*
+import content.region.kandarin.seers_village.quest.murder.MurderMystery
+import core.api.getAttribute
+import core.api.getQuestStage
+import core.api.openDialogue
 import core.game.dialogue.*
 import core.game.node.entity.player.Player
 import core.plugin.Initializable
 import core.tools.END_DIALOGUE
-import shared.consts.NPCs
+import shared.consts.*
 
 @Initializable
 class StanfordDialogue(player: Player? = null) : Dialogue(player) {
@@ -36,37 +36,19 @@ class StanfordDialogueFile : DialogueFile() {
                 2 -> showTopics(
                     Topic("Who do you think is responsible?", 3),
                     Topic("Where were you at the time of the murder?", 5),
-                    IfTopic(
-                        "Did you hear any suspicious noises at all?",
-                        6,
-                        getAttribute(player!!, attributeNoiseClue, false)
-                    ),
-                    IfTopic(
-                        "Do you know why so much poison was bought recently?",
-                        11,
-                        getAttribute(player!!, attributePoisonClue, 0) > 0
-                    )
+                    IfTopic("Did you hear any suspicious noises at all?", 6, getAttribute(player!!, MurderMystery.attributeNoiseClue, false)),
+                    IfTopic("Do you know why so much poison was bought recently?", 11, getAttribute(player!!, MurderMystery.attributePoisonClue, 0) > 0)
                 )
-
                 3 -> npcl("It was Anna. She is seriously unbalanced. She trashed the garden once then tried to blame it on me! I bet it was her. It's just the kind of thing she'd do!").also { stage++ }
-                4 -> npcl("She really hates me and was arguing with Lord Sinclair about trashing the garden a few days ago.").also {
-                    stage = END_DIALOGUE
-                }
-
-                5 -> npcl("Right here, by my little shed. It's very cosy to sit and think in.").also {
-                    stage = END_DIALOGUE
-                }
-
+                4 -> npcl("She really hates me and was arguing with Lord Sinclair about trashing the garden a few days ago.").also { stage = 2 }
+                5 -> npcl("Right here, by my little shed. It's very cosy to sit and think in.").also { stage = 2 }
                 6 -> npcl("Not that I remember.").also { stage++ }
                 7 -> playerl("So no sounds of a struggle between Lord Sinclair and an intruder?").also { stage++ }
                 8 -> npcl("Not to the best of my recollection.").also { stage++ }
                 9 -> playerl("How about the guard dog barking?").also { stage++ }
                 10 -> npcl("Not that I can recall.").also { stage = END_DIALOGUE }
-
                 11 -> npcl("Well, Bob mentioned to me the other day he wanted to get rid of the bees in that hive over there. I think I saw him buying poison").also { stage++ }
-                12 -> npcl("from that poison salesman the other day. I assume it was to sort out those bees. You'd really have to ask him though.").also {
-                    stage = END_DIALOGUE
-                }
+                12 -> npcl("from that poison salesman the other day. I assume it was to sort out those bees. You'd really have to ask him though.").also { stage = 2 }
             }
 
             100 -> npcl("Thank you for all your help in solving the murder.").also { stage = END_DIALOGUE }
